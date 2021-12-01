7e48ffed3fcecf7b0170f010f50fcacc48b636c5
daniel ouattara <daniel.ouattara@gmx.com>
2021-12-01 08:50:06 +0100

    fetching & listing data from *.md using graphql

---
+++ 13_Fetching_&_Listing_Data/src/pages/projects/index.js
@@ -0,0 +1,45 @@
+// import { Link } from 'gatsby';
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Layout from './../../components/Layout';
+import * as styles from './../../styles/project.module.css';
+
+export default function Projects({ data }) {
+    console.log(data);
+    const projects = data.allMarkdownRemark.nodes
+    return (
+        <Layout >
+            <div className={styles.portfolio}>
+                <h2>Portfolio</h2>
+                <h3>My Projects &amp; Websites </h3>
+                <div className={styles.projects}>{projects.map((project) => 
+                    <Link to={"/porjects/"+project.frontmatter.slug} key={project.id}>
+                        <div>
+                            <h3>{project.frontmatter.title}</h3>
+                            <p>{project.frontmatter.stack}</p>
+                        </div>
+                    </Link >
+                
+                 )}</div>
+            </div>
+        </Layout>
+    );
+}
+
+/* export page query 
+---------------------*/
+
+export const query = graphql`
+query ProjectsPage {
+    allMarkdownRemark {
+        nodes {
+            frontmatter {
+                slug
+                stack
+                title
+            }
+            id
+        }
+    }
+}
+`
