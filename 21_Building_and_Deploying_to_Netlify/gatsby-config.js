428cb2f33404942bd6abc0d30b79712264d7371c
daniel ouattara <daniel.ouattara@gmx.com>
2021-12-02 07:05:09 +0100

    gatsby build: OK

---
+++ 21_Building_and_Deploying_to_Netlify/gatsby-config.js
@@ -0,0 +1,35 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    'gatsby-transformer-remark',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "Web Warrior",
+    description: "Web Development Portfolio",
+    copyright: "This website is copyright 2021 Web Warrior",
+    contact: "daniel.ouattara@gmx.com"
+  }
+}
